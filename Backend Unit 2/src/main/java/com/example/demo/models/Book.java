e00dee0dc546c6b2e4fb561397e1428589b6a8d1
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-10 13:27:19 -0500

    Implemented Book entity based on fields in React app, also imported Lombok

--- Backend Unit 2/src/main/java/com/example/demo/models/Book.java
+++ Backend Unit 2/src/main/java/com/example/demo/models/Book.java
@@ -1,4 +1,23 @@
 package com.example.demo.models;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Data
 
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String title;
+    private String author;
+    private String spineColor;
+    private String status;
+    private String description;
+    //user submitted book recs
+    private String note;
+    private String name;
 }
