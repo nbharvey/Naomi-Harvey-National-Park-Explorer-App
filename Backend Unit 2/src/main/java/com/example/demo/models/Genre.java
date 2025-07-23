ff132d6cb2bd0d280d799b54aae176375833eb83
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-23 12:08:04 -0500

    added entity annotation to genre entity

--- Backend Unit 2/src/main/java/com/example/demo/models/Genre.java
+++ Backend Unit 2/src/main/java/com/example/demo/models/Genre.java
@@ -1,9 +1,11 @@
 package com.example.demo.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+@Entity
 public class Genre {
 
     @Id
