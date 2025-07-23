ffca48fc0406342080f6fa20fd2910de42431e05
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-23 13:13:19 -0500

    implemented GenreController

--- Backend Unit 2/src/main/java/com/example/demo/controllers/GenreController.java
+++ Backend Unit 2/src/main/java/com/example/demo/controllers/GenreController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Genre;
+import com.example.demo.services.GenreService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+@RestController
+@RequestMapping("/genres")
 public class GenreController {
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    //@return a JSON array of all genres
+
+    @GetMapping
+    public List<Genre> getAllGenres() {
+        return genreService.getAllGenres();
+    }
 }
