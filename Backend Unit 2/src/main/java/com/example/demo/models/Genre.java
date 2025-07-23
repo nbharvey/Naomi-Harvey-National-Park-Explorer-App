4f20f1a4fb53ae2374f3a3d14ccdc9ab92357480
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-23 11:51:07 -0500

    Implemented Genre entity

--- Backend Unit 2/src/main/java/com/example/demo/models/Genre.java
+++ Backend Unit 2/src/main/java/com/example/demo/models/Genre.java
@@ -1,4 +1,15 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
 }
