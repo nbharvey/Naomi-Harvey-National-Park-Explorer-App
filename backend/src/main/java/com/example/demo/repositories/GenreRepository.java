215323ab351d5e5cbcd8e22ed948fba33c3a4812
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-29 12:55:18 -0500

    added seed file

--- backend/src/main/java/com/example/demo/repositories/GenreRepository.java
+++ backend/src/main/java/com/example/demo/repositories/GenreRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repositories;
 import com.example.demo.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    Optional<Genre> findByName(String name);
 }
