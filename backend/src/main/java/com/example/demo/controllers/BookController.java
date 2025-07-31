feba37f02397016684b796568facfdd0eb932abe
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-07-30 20:22:20 -0500

    imported CrossOrigin

--- backend/src/main/java/com/example/demo/controllers/BookController.java
+++ backend/src/main/java/com/example/demo/controllers/BookController.java
@@ -5,6 +5,7 @@ import com.example.demo.services.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Map;
