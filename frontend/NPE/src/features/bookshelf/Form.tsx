62d6f5bff131212db02fd7e0dbf349a6d08580b2
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-08-01 11:22:49 -0500

    created NewBookData interface to represent data from the form users submit to create a new book

--- frontend/NPE/src/features/bookshelf/Form.tsx
+++ frontend/NPE/src/features/bookshelf/Form.tsx
@@ -5,7 +5,6 @@ import '../../index.css';
 import type { Genre } from '../../types';
 
 
-//type for genre object being fetched from API
 
 
 interface FormProps {
--- frontend/NPE/src/types/index.ts
+++ frontend/NPE/src/types/index.ts
@@ -1,16 +1,29 @@
+//type for genre object being fetched from API
 interface Genre {
   id: number;
   name: string;
 }
 
+//represents a complete Book object as exists in the db
 export interface BookData {
     id: number;
     title: string;
     author: string;
-    genreIDs: number[];
+    genreIds: number[];
     description: string;
     spineColor: string;
     name?: string;
     isEditing?: boolean;
+    note?: string;
+  }
+
+  //represents data the form submits to create a book
+  export interface NewBookData {
+    title: string;
+    author: string;
+    genreIds: number[];
+    description: string;
+    spineColor: string;
+    name?: string;
     note?: string;
   }
