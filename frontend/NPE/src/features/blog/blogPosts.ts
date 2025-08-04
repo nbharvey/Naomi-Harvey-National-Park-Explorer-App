793cf70e669d54438616646e2b32da5583ac878c
Naomi Harvey <naomi.b.harvey@gmail.com>
2025-08-04 11:00:44 -0500

    created content for Blog

---
+++ frontend/NPE/src/features/blog/blogPosts.ts
@@ -0,0 +1,13 @@
+import { BlogPost } from "./types";
+
+export const blogPosts: BlogPost[] = [
+    {
+        id: 'epic-western-road-trip-part-1',
+        title: 'Epic Western Road Trip Part 1',
+        imageURL: '',
+        excerpt: '',
+        content: '',
+        author: 'Naomi Harvey',
+        date: 'July 01, 2022',
+    },
+];
---
+++ frontend/NPE/src/features/blog/types.ts
@@ -0,0 +1,9 @@
+export interface BlogPost {
+    id: string;
+    title: string;
+    imageURL: string;
+    excerpt: string;
+    content: string;
+    author: string;
+    date: string;
+}
